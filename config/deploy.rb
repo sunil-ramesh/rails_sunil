c09774787650624eca1cb0769f3067fa1e888a38
sunil-ramesh <skramesh@qwinix.io>
2016-12-14 19:06:47 +0530

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.7.0'
 
-set :application, 'event'
+set :application, 'rails_sunil'
 set :repo_url, 'https://github.com/sunil-ramesh/rails_sunil.git'
 
 # Default branch is :master
@@ -23,7 +23,7 @@ set :repo_url, 'https://github.com/sunil-ramesh/rails_sunil.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
