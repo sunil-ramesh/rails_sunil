c648541518354ade3c0cad6d678ebb9f67d75e7f
sunil-ramesh <skramesh@qwinix.io>
2016-12-14 19:28:58 +0530

    changes

--- app/models/action.rb
+++
@@ -1,5 +0,0 @@
-class Action < ActiveRecord::Base
-	belongs_to :owner,class_name: "User"
-	has_many :participants, dependent: :destroy
-	has_many :users,through: :participants
-end
--- db/migrate/20161129045326_add_reset_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddResetToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :reset_digest, :string
-  end
-end
